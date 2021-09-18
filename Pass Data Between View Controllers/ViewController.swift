8ef3759bf731468aea736d1282417c0f255bbf90
kimjunsoo <rlawnstn01023@naver.com>
2021-09-18 20:12:14 +0900

    Initial Commit

---
+++ Pass Data Between View Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pass Data Between View Controllers
+//
+//  Created by 준수김 on 2021/09/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
